44607dbee4affd210e7988f2f6baf63702f09997
quepas <kiepas@agh.edu.pl>
2017-07-28 21:29:28 +0200

    Simplification of all three functions.

--- MAPI_create.c
+++ MAPI_create.c
@@ -3,12 +3,12 @@
 #include "papi.h"
 
 #include "MAPI_status.h"
+
 /*
  *  MAPI_create
  */
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
     int papi_num_counters = PAPI_num_counters();
-    
     if (nrhs < 1 || nrhs > papi_num_counters) {
         char error[128];
         sprintf(error, "MAPI_create takes from 1 to %d "
@@ -17,23 +17,16 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
         mexErrMsgTxt(error);
     }
 
-    /* Stop all running counters */
-    if (MAPI_getGlobalStatus() == MAPI_STATUS_COUNTING) {
-        mexPrintf("Stopping all current counters");
-        PAPI_stop_counters(NULL, 0);
-    }
+    /* Stop all available counters (even not running ones) */
+    long long mock_values[papi_num_counters];
+    PAPI_stop_counters(mock_values, papi_num_counters);
 
-    /* Set new global state */
-    MAPI_setGlobalStatus(MAPI_STATUS_INIT);
-
-    /* Create an event code vector */
-    int* events = mxCalloc(nrhs, sizeof(INT32_T));
 
     /* Set an output vector */
     plhs[0] = mxCreateNumericMatrix(1, nrhs, mxINT32_CLASS, mxREAL);
-    int* out_vector = mxGetData(plhs[0]);
+    int* counters = mxGetData(plhs[0]);
 
-    int i;
+    int i, result;
     for (i = 0; i < nrhs; ++i) {
         if (!mxIsChar(prhs[i])) {
             mexErrMsgTxt("MAPI_create requires a string with hardware counter name.");
@@ -44,15 +37,19 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
         if (mxGetString(prhs[i], buffer, id_length+1) == 1) {
             mexErrMsgTxt("Error while reading a string name.");
         }
-
-        int result;
-        int event;
-        if (result = PAPI_event_name_to_code(buffer, &(event)) != PAPI_OK) {
+        /* Get integer code for each counter */
+        if (result = PAPI_event_name_to_code(buffer, &(counters[i])) != PAPI_OK) {
             char error[256];
             sprintf(error, "Failed to obtained event code %s."
                            "Error message: %s.", buffer, PAPI_strerror(result));
             mexErrMsgTxt(error);
         }
-        out_vector[i] = event;
     }
+    /* Start counters */
+    if ((result = PAPI_start_counters(counters, nrhs)) != PAPI_OK) {
+        char error[128];
+        sprintf(error, "MAPI_tic couldn't start counters. Error message: %s", PAPI_strerror(result));
+        mexErrMsgTxt(error);
+    }
+    MAPI_setGlobalStatus(nrhs);
 }
--- MAPI_toc.c
+++ MAPI_toc.c
@@ -6,27 +6,17 @@
 
 /* PAPI_toc.c - start counters */
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-    if (nrhs < 1) {
-        mexErrMsgTxt("MAPI_toc requires one vector with counter descriptors.");
-    }
-    if (MAPI_getGlobalStatus() != MAPI_STATUS_COUNTING) {
-        mexErrMsgTxt("No active counters. Run MAPI_tic before.");
-    }
-
-    /* Check counter descriptors */
-    unsigned int mrows = mxGetM(prhs[0]);
-    unsigned int ncols = mxGetN(prhs[0]);
-    if (!mxIsInt32(prhs[0]) || mrows > 1 || ncols == 0) {
-        mexErrMsgTxt("MAPI_toc requries a non-empty row vector "
-                     "with counter descriptors (int32).");
-    }
-
-    plhs[0] = mxCreateNumericMatrix(1, ncols, mxINT64_CLASS, mxREAL);
-    long long *counter_values = mxGetData(plhs[0]);
-    int result;
-    if ((result = PAPI_read_counters(counter_values, ncols)) != PAPI_OK) {
-        char error[256];
-        sprintf(error, "Failed to read counters. "
-                       "Error message: %s.", PAPI_strerror(result));
+    int num_counters = MAPI_getNumRunningCounters();
+    if (num_counters > 0) {
+        plhs[0] = mxCreateNumericMatrix(1, num_counters, mxINT64_CLASS, mxREAL);
+        long long *counter_values = mxGetData(plhs[0]);
+        int result;
+        if ((result = PAPI_read_counters(counter_values, num_counters)) != PAPI_OK) {
+            char error[256];
+            sprintf(error, "Failed to read counters. "
+                           "Error message: %s.", PAPI_strerror(result));
+        }
+    } else {
+        mexPrintf("Counters are not running. Execute MAPI_create first.\n");
     }
 }
