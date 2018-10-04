05f5a0e04584674c3d6a9ec9caa076c35bf55bfa
quepas <kiepas@agh.edu.pl>
2018-10-04 12:34:52 +0200

    Holding only the id of current event set

--- mPAPI_status.h
+++ mPAPI_status.h
@@ -2,44 +2,20 @@
 #define _MPAPI_STATUS_H_
 
 #include "mex.h"
+#include <string.h>
 
-#define MPAPI_STATUS_NONE -2
-#define MPAPI_STATUS_INIT -1
-#define MPAPI_STATUS_COUNTING 0
-/* MPAPI_STATUS_1, 2, 3,... number of active counters */
-
-/*
- *  Set global variable mPAPI_status
- */
-void mPAPI_setGlobalStatus(int initial_value)
+void mPAPI_set_event_set(int event_set_id)
 {
-    mxArray *mPAPI_status = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
-    int *status = (int *)mxGetData(mPAPI_status);
-    *status = initial_value;
-    mexPutVariable("global", "mPAPI_status", mPAPI_status);
+    mxArray *variable = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
+    int *value = (int *)mxGetData(variable);
+    *value = event_set_id;
+    mexPutVariable("global", "mPAPI_event_set", variable);
 }
 
-/*
- *  Get global variable MAPI_status
- */
-int mPAPI_getGlobalStatus()
+int mPAPI_get_event_set()
 {
-    mxArray *mPAPI_status = mexGetVariable("global", "mPAPI_status");
-    if (mPAPI_status == 0)
-    {
-        return MPAPI_STATUS_NONE;
-    }
-    return *((int *)mxGetData(mPAPI_status));
-}
-
-int mPAPI_getNumRunningCounters()
-{
-    int current_status = mPAPI_getGlobalStatus();
-    if (current_status > MPAPI_STATUS_COUNTING)
-    {
-        return current_status;
-    }
-    return 0;
+    mxArray *variable = mexGetVariable("global", "mPAPI_event_set");
+    return *((int *)mxGetData(variable));
 }
 
 /* _MPAPI_STATUS_H_ */
