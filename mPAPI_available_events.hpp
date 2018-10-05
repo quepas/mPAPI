7b4362cd90c994583f603b88b9334001de5a73fb
quepas <kiepas@agh.edu.pl>
2018-10-05 02:52:40 +0200

    Adding functions for availabilty testing of performance evnets

---
+++ mPAPI_available_events.hpp
@@ -0,0 +1,66 @@
+#ifndef _MPAPI_AVAILABLE_EVENTS_
+#define _MPAPI_AVAILABLE_EVENTS_
+
+#include <papi.h>
+
+#include <algorithm>
+#include <map>
+#include <vector>
+
+// Function definitions
+void fillOneEventSet(std::multimap<int, int> preset_max_avail,
+                     std::vector<int> &event_codes,
+                     std::vector<int> &event_set_codes)
+{
+    int retval;
+    int event_set = PAPI_NULL;
+    if ((retval = PAPI_create_eventset(&event_set)) != PAPI_OK)
+    {
+        mPAPI_mex_error_with_reason("Failed to create an event set", retval);
+    }
+    int max_counters = PAPI_num_counters();
+    int in_event_set = 1;
+    for (auto &pair : preset_max_avail)
+    {
+        int event_id = pair.second;
+        PAPI_event_info_t info;
+        PAPI_get_event_info(event_id, &info);
+        // Any space left in the EventSet?
+        if (in_event_set > max_counters)
+            break;
+        if (PAPI_add_event(event_set, event_id) == PAPI_OK)
+        {
+            mPAPI_remove_event_code(event_codes, event_id);
+            event_set_codes.push_back(event_id);
+            in_event_set++;
+        }
+    }
+}
+
+void countAvailability(std::vector<int> event_codes,
+                       std::multimap<int, int> &preset_max_avail)
+{
+    int retval;
+    int event_set = PAPI_NULL;
+    if ((retval = PAPI_create_eventset(&event_set)) != PAPI_OK)
+    {
+        mPAPI_mex_error_with_reason("Failed creating an event set", retval);
+    }
+    // Check each event
+    for (int i = 0; i < event_codes.size(); ++i)
+    {
+        PAPI_add_event(event_set, event_codes[i]);
+        // By looking how many other events are compatible with it
+        for (int k = i + 1; k < event_codes.size(); ++k)
+        {
+            PAPI_add_event(event_set, event_codes[k]);
+        }
+        // Check how many is compatible
+        int availableSize = PAPI_num_events(event_set);
+        PAPI_cleanup_eventset(event_set);
+        preset_max_avail.insert({availableSize, event_codes[i]});
+    }
+}
+
+// ~_AVAILABLE_EVENTS_
+#endif
---
+++ mPAPI_utils.hpp
@@ -0,0 +1,89 @@
+#ifndef _MPAPI_STATUS_H_
+#define _MPAPI_STATUS_H_
+
+#include <algorithm>
+#include <vector>
+#include <string>
+#include "mex.h"
+#include "papi.h"
+
+void mPAPI_remove_event_code(std::vector<int> &event_codes, int event_code_to_remove)
+{
+    event_codes.erase(std::find(event_codes
+                                    .begin(),
+                                event_codes
+                                    .end(),
+                                event_code_to_remove));
+}
+
+void mPAPI_set_event_set(int event_set_id)
+{
+    mxArray *variable = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
+    int *value = (int *)mxGetData(variable);
+    *value = event_set_id;
+    mexPutVariable("global", "mPAPI_event_set", variable);
+}
+
+int mPAPI_get_event_set()
+{
+    mxArray *variable = mexGetVariable("global", "mPAPI_event_set");
+    return *((int *)mxGetData(variable));
+}
+
+void mPAPI_mex_error_with_reason(std::string base_error_msg, int reason_id)
+{
+    std::string reason_msg = PAPI_strerror(reason_id);
+    base_error_msg += ". Reason: ";
+    base_error_msg += reason_msg;
+    mexErrMsgTxt(base_error_msg.c_str());
+}
+
+void mPAPI_mex_warn_with_reason(std::string base_error_msg, int reason_id)
+{
+    std::string reason_msg = PAPI_strerror(reason_id);
+    base_error_msg += ". Reason: ";
+    base_error_msg += reason_msg;
+    mexWarnMsgTxt(base_error_msg.c_str());
+}
+
+mxArray *mPAPI_to_string_cell_array(std::vector<std::string> names)
+{
+    mxArray *native_events = mxCreateCellMatrix(1, names.size());
+    for (int i = 0; i < names.size(); ++i)
+    {
+        mxArray *name = mxCreateString(names[i].c_str());
+        mxSetCell(native_events, i, mxDuplicateArray(name));
+        mxDestroyArray(name);
+    }
+    return native_events;
+}
+
+void mPAPI_event_names_cell_to_codes(const mxArray *names_in_cell, int num_events, std::vector<int> &out_codes)
+{
+    if (!mxIsCell(names_in_cell))
+    {
+        mexErrMsgTxt("Argument is not a cell array.");
+    }
+
+    for (int i = 0; i < num_events; ++i)
+    {
+        mxArray *event_name_ptr = mxGetCell(names_in_cell, i);
+        if (mxIsChar(event_name_ptr))
+        {
+            char *event_name = mxArrayToString(event_name_ptr);
+            int event_code;
+            int retval;
+            if ((retval = PAPI_event_name_to_code(event_name, &event_code)) != PAPI_OK)
+            {
+                mPAPI_mex_warn_with_reason("Failed to convert event name to code", retval);
+            }
+            else
+            {
+                out_codes.push_back(event_code);
+            }
+        }
+    }
+}
+
+/* _MPAPI_STATUS_H_ */
+#endif
