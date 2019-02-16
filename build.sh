0cdbc79d781f3f09f23033cff0d5fcaa6824273f
quepas <kiepas.patryk@gmail.com>
2019-02-16 22:07:40 +0100

    Set g++ for C++11

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 MEX=$1
 COMPILER=$2
 OUT_DIR=$3
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_groupEvents.cpp     -L/usr/local/lib/ -lpapi -output mPAPI_groupEvents -outdir $OUT_DIR
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_enumPresetEvents.cpp -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_enumPresetEvents -outdir $OUT_DIR
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_enumNativeEvents.cpp -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_enumNativeEvents -outdir $OUT_DIR
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_register.cpp        -L/usr/local/lib/ -lpapi -output mPAPI_register -outdir $OUT_DIR
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_tic.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_tic -outdir $OUT_DIR
-$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER -I/usr/local/include mPAPI_toc.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_toc -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_groupEvents.cpp     -L/usr/local/lib/ -lpapi -output mPAPI_groupEvents -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_enumPresetEvents.cpp -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_enumPresetEvents -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_enumNativeEvents.cpp -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_enumNativeEvents -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_register.cpp        -L/usr/local/lib/ -lpapi -output mPAPI_register -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_tic.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_tic -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_toc.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_toc -outdir $OUT_DIR
