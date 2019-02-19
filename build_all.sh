6227dc0b1f0f236b28cc720d16680685cae1ee53
quepas <kiepas.patryk@gmail.com>
2019-02-19 22:38:08 +0100

    Copy common-matlab to output directory

--- build_all.sh
+++ build_all.sh
@@ -2,3 +2,8 @@
 ./build.sh '/usr/local/MATLAB/R2013a/bin/mex' '/usr/bin/g++-4.4' 'r2013a/'
 ./build.sh '/usr/local/MATLAB/R2015b/bin/mex' '/usr/bin/g++-4.7' 'r2015b/'
 ./build.sh '/usr/local/MATLAB/R2018b/bin/mex' '/usr/bin/g++-6'   'r2018b/'
+
+# Copy common-matlab to each output directories
+cp -a common-matlab/. r2013a/
+cp -a common-matlab/. r2015b/
+cp -a common-matlab/. r2018b/
