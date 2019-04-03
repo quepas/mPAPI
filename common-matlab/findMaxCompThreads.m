c3b9fe7727640b629d7356e13d20a107c3285859
quepas <kiepas.patryk@gmail.com>
2019-04-03 13:47:20 +0200

    Adding MATLAB routine for finding maximal number of available threads (up to a number)

---
+++ common-matlab/findMaxCompThreads.m
@@ -0,0 +1,9 @@
+% Comment: Hyper-threading might affect the number of available computational threads
+function maxCompThreads = findMaxCompThreads(tryMaxThreads)
+    % Try to setup maximal number of computational threads
+    maxNumCompThreads(tryMaxThreads);
+    % Check how many we have found
+    foundThreads = findCompThreads();
+    % We should find at least two threads (might be not true for uniprocessors)
+    maxCompThreads = max(2, numel(foundThreads));
+end
