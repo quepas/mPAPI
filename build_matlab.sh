b2638a4778d50ed40e087b36ba366183770e3832
quepas <kiepas.patryk@gmail.com>
2019-05-24 20:44:58 +0200

    Adding profile/stop profile methods to building routine

--- build_matlab.sh
+++ build_matlab.sh
@@ -8,3 +8,6 @@ $MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I
 $MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_register.cpp        -L/usr/local/lib/ -lpapi -output mPAPI_register -outdir $OUT_DIR
 $MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_tic.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_tic -outdir $OUT_DIR
 $MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_toc.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_toc -outdir $OUT_DIR
+# Profiling with overflow
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_profile.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_profile -outdir $OUT_DIR
+$MEX CXX=$COMPILER CC=$COMPILER GCC=$COMPILER CXXFLAGS='$CXXFLAGS -std=c++0x' -I/usr/local/include mPAPI_stop.cpp             -L/usr/local/lib/ -lpapi -output $OUT_DIR/mPAPI_stop -outdir $OUT_DIR
