2764e442f297b3b49f732eeff509cad2d04b4032
quepas <kiepas.patryk@gmail.com>
2019-05-31 18:17:11 +0200

    Write explicit start/end point of the (sub)trace

--- mPAPI_trace_toc.cpp
+++ mPAPI_trace_toc.cpp
@@ -11,6 +11,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     int event_set = mPAPI_get_int32_scalar(mxGetFieldByNumber(prhs[0], 0, 0));
     int num_events = mPAPI_get_int32_scalar(mxGetFieldByNumber(prhs[0], 0, 5));
+    std::string trace_file_name = mxArrayToString(mxGetFieldByNumber(prhs[0], 0, 6));
+
     long long values[num_events];
     int retval;
 
@@ -18,4 +20,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         mPAPI_mex_error_with_reason("Failed to start the profiling.", retval);
     if ((retval = PAPI_reset(event_set)) != PAPI_OK)
         mPAPI_mex_error_with_reason("Failed to reset the counters.", retval);
+
+    std::ofstream trace_file(trace_file_name, std::fstream::app);
+    trace_file << "@trace_end" << std::endl;
+    trace_file.close();
 }
